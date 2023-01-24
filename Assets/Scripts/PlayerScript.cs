6e8bbfdddf8aa12c339163927e7723a5ecb4b118
Ilan Velter <ilan.velter@etu.umontpellier.fr>
2023-01-24 17:04:52 +0100

    physiques d'eau pétées

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -1,20 +1,31 @@
+using System;
 using UnityEngine;
 
 public class PlayerScript : MonoBehaviour
 {
+    //Movement variables
     public float moveSpeed;
     public float jumpForce;
 
     public Transform groundCheckLeft;
     public Transform groundCheckRight;
+    public Transform waterCheck;
     
     private bool isJumping;
     private bool auSol;
-    
+    private bool onSurfaceWater = false;
+    private bool jumpingOutOfWater = false;
+
     public Rigidbody2D rb;
     public SpriteRenderer spriteRenderer;
     //public Animator anime;
     private Vector3 velocity = Vector3.zero;
+    
+    //Season changes variables
+    private bool unlockedNormal = true;
+    private bool unlockedSnow = true;
+    private bool unlockedRain = true;
+    private bool unlockedHeatWave = true;
 
     private void FixedUpdate()
     {
@@ -23,25 +34,36 @@ public class PlayerScript : MonoBehaviour
         if(Input.GetButton("Jump") && auSol == true) {
             isJumping = true;
         }
-        Debug.Log(isJumping);
-
         MPlayer(horizontalMovment);
         Flip(rb.velocity.x);
         float absVelocityX = Mathf.Abs(rb.velocity.x);
+        
         //anime.SetFloat("speed",absVelocityX);
+        Debug.Log(onSurfaceWater);
     }
 
-    void MPlayer(float horizontalMovment) {
+    private void MPlayer(float horizontalMovment) {
         Vector3 targetVelocity = new Vector2(horizontalMovment,rb.velocity.y);
         rb.velocity = Vector3.SmoothDamp(rb.velocity, targetVelocity, ref velocity, 0.05f);
-        if (isJumping == true)
+        if (jumpingOutOfWater && auSol && !onSurfaceWater)
+        {
+            jumpingOutOfWater = false;
+        }
+        
+        if (isJumping && !onSurfaceWater && !jumpingOutOfWater)
         {
             rb.AddForce(new Vector2(0f,jumpForce));
             isJumping = false;
         }
+        else if (isJumping && onSurfaceWater)
+        {
+            rb.AddForce(new Vector2(0f,jumpForce/10));
+            jumpingOutOfWater = true;
+            isJumping = false;
+        }
     }
 
-    void Flip(float _velocity)
+    private void Flip(float _velocity)
     {
         if (_velocity > 0.1f)
         {
@@ -50,6 +72,22 @@ public class PlayerScript : MonoBehaviour
         else if (_velocity < -0.1f)
         {
             spriteRenderer.flipX = true;
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.gameObject.layer == 4)
+        {
+            onSurfaceWater = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.layer == 4)
+        {
+            onSurfaceWater = false;
         }
     }
 }
